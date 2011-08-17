b1d5a7257b030e8b762fd88e4a9a1b1819b13805
Ronny Haryanto <ronny@haryan.to>
2011-08-17 10:23:51 +1000

    no more rbenv, back to rvm

--- custom/rvm.zsh
+++ custom/rvm.zsh
@@ -1,7 +1,1 @@
-# Ruby RVM.
-
-# This loads RVM into a shell session.
-if [[ -s "$HOME/.rvm/scripts/rvm" ]]; then
-    source "$HOME/.rvm/scripts/rvm"
-    __rvm_project_rvmrc
-fi
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
--- lib/rbenv.zsh
+++ lib/rbenv.zsh
@@ -1,5 +1,5 @@
 # get the name of the ruby we are on
 function ruby_prompt_info() {
-  ruby_version=$(ruby -v 2> /dev/null | awk '{print $1, $2}') || return
+  ruby_version=$(ruby -v 2> /dev/null | awk '{print $1, $2}')
   echo "($ruby_version)"
 }
